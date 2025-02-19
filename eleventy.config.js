7e20d8928137aadeae63d616a257c1ff92920a93
CelDaemon <celdaemon@voidgroup.net>
2025-02-19 22:11:06 +0100

    Add svgs, orange stars, and embeds

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 export default async function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("robots.txt");
-	eleventyConfig.addWatchTarget("**/*.{css,js}");
+	eleventyConfig.addPassthroughCopy("**/*.svg");
+	eleventyConfig.addWatchTarget("**/*.{css,js,svg}");
 	eleventyConfig.addFilter("forceCapitalize", value => {
 		value ??= '';
 		return value.charAt(0).toUpperCase() + value.slice(1)
