1e80a07b0ba69371f023e08209451cc0e03f0ca9
CelDaemon <celdaemon@voidgroup.net>
2025-02-19 13:21:07 +0100

    Add tags

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,8 @@
 export default async function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("robots.txt");
 	eleventyConfig.addWatchTarget("**/*.{css,js}");
+	eleventyConfig.addFilter("forceCapitalize", value => {
+		value ??= '';
+		return value.charAt(0).toUpperCase() + value.slice(1)
+	})
 }
