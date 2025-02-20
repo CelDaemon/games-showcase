97f68ef18947e82acc752d81c17f5d71eef15145
CelDaemon <celdaemon@voidgroup.net>
2025-02-20 17:38:59 +0100

    Clean up css and icons

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,22 @@
-function test() {
-
+import slugify from "@sindresorhus/slugify";
+function lens(object, path) {
+	return path.split(".").reduce((object, key) => object && object[key] ? object[key] : null, object);
 }
 
 export default async function(eleventyConfig) {
-	const lens = (object, path) => path.split(".").reduce((o, key) => o && o[key] ? o[key] : null, object);
 	eleventyConfig.addFilter("contains", (object, value, key = '') => lens(object, key)?.includes(value));
-	eleventyConfig.addFilter("flatMap", (object, key) => lens(object, key));
+	eleventyConfig.addFilter("flatMap", lens);
 	eleventyConfig.addPassthroughCopy("robots.txt");
-	eleventyConfig.addPassthroughCopy("img/*.svg");
-	eleventyConfig.addWatchTarget("**/*.{css,js,svg}");
 	eleventyConfig.addFilter("forceCapitalize", value => {
 		value ??= '';
-		return value.charAt(0).toUpperCase() + value.slice(1)
+		return value.charAt(0).toUpperCase() + value.slice(1);
 	});
+	eleventyConfig.addFilter("gameUrl", game => {
+		if(!game || typeof game.name !== "string") throw new Error("Could not generate game url for invalid game");
+		return `/game/${slugify(game.name)}/`;
+	});
+	eleventyConfig.addFilter("tagUrl", tag => {
+		if(typeof tag !== "string") throw new Error("Could not generate tag url for invalid tag");
+		return `/tag/${slugify(tag)}/`;
+	})
 }
