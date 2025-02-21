ce7e3b051ea5f7ff4c84600e0bcd63f23946124c
CelDaemon <celdaemon@voidgroup.net>
2025-02-21 14:16:08 +0100

    Add commit to page

--- _data/site.js
+++ _data/site.js
@@ -1,3 +1,3 @@
 import process from 'process';
 console.log(`Site URL: ${process.env.SITE_URL}`);
-export default {url: process.env.SITE_URL};
+export default {url: process.env.SITE_URL, sourceUrl: process.env.SOURCE_URL, sourceCommit: process.env.SOURCE_COMMIT};
