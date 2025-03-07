c14902f6bdfe72f831c0616092644578ce537917
CelDaemon <celdaemon@voidgroup.net>
2025-03-07 11:10:32 +0100

    Use insecure url for opengraph

--- _data/site.js
+++ _data/site.js
@@ -1,2 +1,2 @@
 import process from 'process';
-export default {url: process.env.SITE_URL || "http://localhost:8080", repositoryUrl: process.env.REPOSITORY_URL};
+export default {url: process.env.SITE_URL || "http://localhost:8080", insecureUrl: process.env.SITE_INSECURE_URL || "http://localhost:8080", repositoryUrl: process.env.REPOSITORY_URL};
