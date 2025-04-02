fd712043bffb0ce96689f03853899b55e77345a9
CelDaemon <celdaemon@voidgroup.net>
2025-04-02 09:18:46 +0200

    Run prettier on generated svg and xml files

--- eleventy.config.js
+++ eleventy.config.js
@@ -64,7 +64,7 @@ export default async function(eleventyConfig) {
 	}));
 	eleventyConfig.addTransform("prettier", (content, output) => {
 		const extension = extname(output);
-		if(extension !== ".html") return content;
+		if(extension !== ".html" && extension !== ".xml" && extension !== ".svg") return content;
 		return format(content, { parser: 'html', htmlWhitespaceSensitivity: 'ignore' });
 	});
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin,
